27ba99ae2826a9f2265129b246102c933a008ad8
janapriya <chikun525@gmail.com>
2018-08-08 01:07:35 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+         path: 'about-us',
+         component: AboutusComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+     },
+     {
+      path: '',
+      component: HomeComponent
+     }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
